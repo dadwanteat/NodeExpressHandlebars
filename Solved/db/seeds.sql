79d2a1a8f2317088ccb0f26323af87c4ea04749f
Robert Dalton <dalton.robertw@gmail.com>
2020-10-17 00:39:10 -0700

    burger updates

--- Solved/db/seeds.sql
+++ Solved/db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO burgers (name) VALUES ('Bacon Cheese');
+INSERT INTO burgers (name) VALUES ('Blue Cheese');
+INSERT INTO burgers (name, eaten ) VALUES ('Communication Break-Down', true);
+INSERT INTO burgers (name, eaten ) VALUES ('Cheese', true);
+INSERT INTO burgers (name, eaten) VALUES ('Veggie', true);
+INSERT INTO burgers (name) VALUES ('Buffalo');
