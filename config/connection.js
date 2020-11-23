a40d09b72266a792e5d196357130edf8bc1944d4
Robert Dalton <dalton.robertw@gmail.com>
2020-11-23 00:15:55 -0800

    file updates for deployment

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,28 @@
+  
 // Set up MySQL connection.
 var mysql = require("mysql");
+let connection;
+// If there is a jawsdb connection, then use that connection, if not use my local connection
 
-var connection = mysql.createConnection({
+if(process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
+  connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "password",
-    database: "burger_db"
-});
+    database: "burgers_db"
+  });
+}
 
 // Make connection.
 connection.connect(function(err) {
-    if (err) {
-        console.error("error connecting: " + err.stack);
-        return;
-    }
-    console.log("connected as id " + connection.threadId);
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
 });
 
 // Export connection for our ORM to use.
